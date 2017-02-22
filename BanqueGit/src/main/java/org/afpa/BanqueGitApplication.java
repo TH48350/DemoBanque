0c0b89914a6e27ca1ff3d2ac85b1e426d08ad90e
TH48350 <timsit.hicham@gmail.com>
2017-02-22 11:20:33 +0100

    premier commit

---
+++ BanqueGit/src/main/java/org/afpa/BanqueGitApplication.java
@@ -0,0 +1,12 @@
+package org.afpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueGitApplication.class, args);
+	}
+}
