580239f61240476f9a77576adaea64b5f6fb59f4
TH48350 <timsit.hicham@gmail.com>
2017-02-22 11:30:21 +0100

    premiere branche par developpeur1

---
+++ BanqueGit/src/main/java/org/afpa/main/ClassBranche1.java
@@ -0,0 +1,12 @@
+package org.afpa.main;
+
+public class ClassBranche1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Premiere branche1");
+
+	}
+
+}
